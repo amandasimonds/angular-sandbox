dbd8c0b2abacf6d4163e45571e2197a920deaebc
amandasimonds <amandasimonds9@gmail.com>
2022-01-20 11:49:35 -0700

    i learned a lodash thing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { BoundElementProperty } from '@angular/compiler';
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  myList = [
+    'Water',
+    'Vitamins',
+    'Apple',
+    'Probiotics',
+    'Coconut Oil'
+  ]
+
+  bundy = {
+    leg: 4,
+    fur: {
+      striped: {
+        color1: 'red',
+        color2: 'brown'
+      }
+    }
+  }
+
+  bean = {
+    leg: 4,
+    fur: 'tan'
+  }
+
+  color = _.get(this.bean, "fur", this.bean.fur)
+
+  ngOnInit(): void {
+    console.log(this.color);
+  }
+
+  public arrayStuff() {
+    // const myNewArray = _.chunk(this.myList, 3)
+    const myNewArray = _.difference(this.myList, ['Water', 'Apple'])
+    console.log(myNewArray);
+  }
   
 }
